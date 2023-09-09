a5f1ba3a00a0d213a3fa0f3a2483cb2d687a179a
artugal28373 <58047037+artugal28373@users.noreply.github.com>
2023-09-10 01:27:11 +0600

    Update mx_flow-min_cut.cpp

--- mx_flow-min_cut.cpp
+++ mx_flow-min_cut.cpp
@@ -1,6 +1,10 @@
 // a
 // https://lightoj.com/problem/binary-matrix // edges manipulation... block some edges
+
 // https://lightoj.com/problem/angry-programmer // mincut
+
 // https://lightoj.com/problem/space-shuttle-experiments // using mincut (profit - loss) mx sum 
 //profit is to the source side, ,loss is to the sink side, profit > loss then loss is cut, profit < loss  profit is cut
 // so profit - loss = profit - cut  // profit < loss, it is not tken // profit > loss it is tken.
+
+//
