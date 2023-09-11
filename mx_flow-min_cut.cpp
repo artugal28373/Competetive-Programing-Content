5871f3f6085e8b48924267e5862464dd90703c34
artugal28373 <58047037+artugal28373@users.noreply.github.com>
2023-09-11 17:35:50 +0600

    Update mx_flow-min_cut.cpp

--- mx_flow-min_cut.cpp
+++ mx_flow-min_cut.cpp
@@ -7,4 +7,8 @@
 //profit is to the source side, ,loss is to the sink side, profit > loss then loss is cut, profit < loss  profit is cut
 // so profit - loss = profit - cut  // profit < loss, it is not tken // profit > loss it is tken.
 
-//
+// Codechef - maxflow problem
+
+// assign problem - https://www.codechef.com/problems/CALLSCHE
+
+// Minimum vertext cover / mx indepented set - https://www.codechef.com/problems/ADAPWN?tab=statement
