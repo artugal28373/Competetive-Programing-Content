cc6d57c7520f1401ae641a81d84e3f95b3ca84ca
artugal28373 <58047037+artugal28373@users.noreply.github.com>
2023-09-14 17:25:10 +0600

    Update CRT.cpp

--- NumberTheory/CRT.cpp
+++ NumberTheory/CRT.cpp
@@ -1,1 +1,15 @@
+// Granner algorithm/ mixed radix formula  O(k&k)
+// extended gcd to solve this at a time 2 only O(k*lg(lcm(mods...)))
+// cp algo CRT section
+
+// Big integer liner addition, subtration, multiplication
+
+// (a%m1)%m = a%m iff m1%m==0
+
+//  x congurance y1%m1
+//  x congurance y2%m2
+//  x congurance y3%m3
+//  x cogurance  y%lcm(m1,m2, m3) 
+
+// if you have cogrunace with x relation & k then you can say x%k x independentlly if lcm(mods....)%k==0 
 
