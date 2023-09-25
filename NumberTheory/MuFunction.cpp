92f3e3c65824fe0cc4280f9ca18d17be894ab877
artugal28373 <58047037+artugal28373@users.noreply.github.com>
2023-09-26 01:22:19 +0600

    Update MuFunction.cpp

--- NumberTheory/MuFunction.cpp
+++ NumberTheory/MuFunction.cpp
@@ -1,1 +1,15 @@
+// Mu(1) = 1
+// Mu(n) = 0 if n has some prime factor more than once
+// Mu(n) = (-1)^k  here k = number of prime factor
+
+// sum of Mu(d) = 1 if n ==1 otherwise 0 here   d belongs to set Divisor, here Divisor is set of divisor of n // special when gcd of something need 1
+// sum of  f(n) = sum of g(d), d are divisor of n
+//  g(n) = sum od Mu(d)*f(n/d) , d are divisor of n
+
+// e(n) = sum of Mu(d) over n
+// id(n) = n
+// idk(n) = n^k
+// i(n) = 1
+// Phi(n)
+
 // https://codeforces.com/problemset/problem/1559/E
