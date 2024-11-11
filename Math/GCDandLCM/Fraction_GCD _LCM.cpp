d1eaed26ae8ef22669dfe9dde05442dc4beebf70
artugal28373 <58047037+artugal28373@users.noreply.github.com>
2024-11-11 23:21:15 +0600

    Create Fraction_GCD _LCM.cpp

---
+++ Math/GCDandLCM/Fraction_GCD _LCM.cpp
@@ -0,0 +1,12 @@
+https://math.stackexchange.com/questions/44836/gcd-and-lcm-of-fractions
+
+1. Simplify each fraction
+
+2. LCM of two fractions= GCD of denominators / LCM of numerators
+
+3. GCD of two fractions= LCM of denominators / GCD of numerators
+
+
+​
+
+​
